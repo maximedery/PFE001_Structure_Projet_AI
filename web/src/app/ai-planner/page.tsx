1715e22b89e0e0f4fccab5f59f8e6676360bca7c
Samuel Croteau <scroteau94@gmail.com>
2024-05-27 15:52:37 -0400

    Add section with search

--- web/src/app/ai-planner/page.tsx
+++ web/src/app/ai-planner/page.tsx
@@ -4,9 +4,18 @@ import Gantt from '@/components/app/gantt';
 import ScoreHeader from '@/components/app/score-header';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { getTailwindColorValue } from '@/helpers/getTailwindColorValue';
-import { Check, Sparkles, Circle } from 'lucide-react';
+import {
+  Check,
+  Sparkles,
+  Search,
+  Settings,
+  UnfoldVertical,
+  FoldVertical,
+  ChevronDown,
+} from 'lucide-react';
 
 export default function AiPlanner() {
   return (
@@ -55,8 +64,42 @@ export default function AiPlanner() {
         </Tabs>
       </TabsContent>
       <TabsContent value="advanced">Advanced</TabsContent>
-      <div className="border-t text-slate-700 bg-slate-50 text-xs pl-2 py-1">
+      <div className="border-t text-slate-700 bg-slate-50 text-xs px-2 py-1">
         Timeline
+      </div>
+      <div className="flex p-2 gap-2">
+        <div className="w-[250px]">
+          <Input
+            startIcon={Search}
+            placeholder="Search"
+            className="w-[250px]"
+          />
+        </div>
+        <Button variant="outline_black" size={'sm'}>
+          <Settings
+            size={18}
+            color={getTailwindColorValue('slate-950')}
+            className="mr-2"
+          />
+          Settings
+        </Button>
+        <Button variant="outline_black" size={'sm'}>
+          <UnfoldVertical
+            size={18}
+            color={getTailwindColorValue('slate-950')}
+          />
+        </Button>
+        <Button variant="outline_black" size={'sm'}>
+          <FoldVertical size={18} color={getTailwindColorValue('slate-950')} />
+        </Button>
+        <Button variant="outline_black" size={'sm'}>
+          Year / Month
+          <ChevronDown
+            size={18}
+            color={getTailwindColorValue('slate-950')}
+            className="ml-2"
+          />
+        </Button>
       </div>
       <Gantt />
     </Tabs>
